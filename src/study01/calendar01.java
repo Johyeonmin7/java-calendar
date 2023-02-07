b7592800891037bf8de3eb02eea4c612bac6b8e6
Cyber <Cyber@host.docker.internal>
2023-02-07 17:28:53 +0900

    maxday 찾기

--- src/study01/calendar01.java
+++ src/study01/calendar01.java
@@ -1,16 +1,27 @@
 package study01;
 
+import java.util.Scanner;
+
 public class calendar01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ì¼ ì›” í™” ìˆ˜ ëª© ê¸ˆ í† ");
+		System.out.println("ÀÏ ¿ù È­ ¼ö ¸ñ ±İ Åä");
 		System.out.println("------------------");
 		System.out.println("1  2  3  4  5  6  7");
 		System.out.println("8  9  10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 		
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("´ŞÀ» ÀÔ·ÂÇÏ¼¼¿ä");
+		int month = scanner.nextInt();
+		
+		int[] maxDays = {31,28,31,30,31,30,31,31,30,31,30,31};
+		
+		System.out.printf("%d¿ùÀº %dÀÏ±îÁö ÀÖ½À´Ï´Ù %n",month,maxDays[month-1]);
+		scanner.close();
 	}
 
 }
